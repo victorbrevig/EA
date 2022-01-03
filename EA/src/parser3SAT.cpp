3e89792848dc524a65887705e5beaabfadabf5e6
victorbrevig <victorbrevig@gmail.com>
2022-01-03 10:44:30 +0100

    nothing

--- EA/src/parser3SAT.cpp
+++ EA/src/parser3SAT.cpp
@@ -1,12 +1,36 @@
 #include "pch.h"
 #include "parser3SAT.h"
+#include "utils.h"
 namespace Utils
 {
     namespace Parser
     {
 
         std::vector<int> getVector3SAT(const std::string& filePath) {
+            std::string fileString = Files::ReadFile(filePath);
+
+
+
+            std::vector<int> res;
+
             
+            //std::string line = ReadLine();
+
+        
+            //std::vector<string> words{};
+
+
+
+
+
+
+
+
+
+            
+
+
+
         }
 
 
