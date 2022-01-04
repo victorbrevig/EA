9900f6547c5dee8ffbcf6fdb9ac15431ce5cc239
Phillip Løjmand <philse73@gmail.com>
2022-01-04 09:59:25 +0100

    order crossover done need s testing (Victor)

--- EA/src/tspPermutation.cpp
+++ EA/src/tspPermutation.cpp
@@ -6,7 +6,6 @@
 #include <stdlib.h>
 
 
-
 TSPpermutation::TSPpermutation(const Graph& graph)
 	: order(graph.GetNumberOfVertices())
 {
@@ -19,6 +18,13 @@ TSPpermutation::TSPpermutation(const Graph& graph)
 
 }
 
+TSPpermutation::TSPpermutation(const Graph& graph, const std::vector<uint32_t>& _order)
+{
+	order = _order;
+	updateFitness(graph);
+}
+
+
 void TSPpermutation::mutate_2OPT()
 {
 }
@@ -28,42 +34,41 @@ void TSPpermutation::updateFitness(const Graph& graph)
 	fitness = graph.calculateDistByOrder(order);
 }
 
-void TSPpermutation::orderCrossover(const TSPpermutation& matePermutation)
+TSPpermutation TSPpermutation::orderCrossover(const TSPpermutation& firstPerm, const TSPpermutation& secondPerm, const Graph& graph)
 {
-	std::vector<uint32_t> child(order.size());
+	int permSize = firstPerm.order.size();
+	std::vector<uint32_t> childOrder(permSize);
 
 	// boolean vector to check if an element has been inserted in the child permutation
-	std::vector<bool> bVec(order.size());
+	std::vector<bool> bVec(permSize);
 	std::fill(bVec.begin(), bVec.end(), false);
 
-	
-
 	// random index between 0 and permutation size - 1 (inclusive)
-	unsigned int l = rand() % order.size();
+	unsigned int l = rand() % permSize;
 
 	// random index between 0 and l - 1 (inclusive)
 	unsigned int k = (l == 0) ? 0 : rand() % l;
 
 	// values from index k to l (inclusive) are assigned to the same as the first parent
 	for (unsigned int i = k; i <= l; i++) {
-		child[i] = order[i];
-		bVec[order[i]] = true;
+		childOrder[i] = firstPerm.order[i];
+		bVec[firstPerm.order[i]] = true;
 	}
 
 	// fill from second parent starting at l+1 
 	unsigned int yIndex = l + 1;
-	for (unsigned int i = l + 1; i < k; i = (i + 1) % order.size()) {
-		if (!bVec[matePermutation.order[i]]) {
-			child[i] = matePermutation.order[i];
-			bVec[matePermutation.order[i]] = true;
-			yIndex++;
+	for (unsigned int i = l + 1; i < k; i = (i + 1u) % permSize) {
+		while (!bVec[secondPerm.order[yIndex]]) {
+			yIndex = (yIndex + 1u) % permSize;
 		}
-		else {
-
-		}
+		childOrder[i] = secondPerm.order[yIndex];
+		bVec[secondPerm.order[yIndex]] = true;
+		yIndex = (yIndex + 1u) % permSize;
 	}
 
+	return TSPpermutation(graph, childOrder);
 }
+
 
 
 
